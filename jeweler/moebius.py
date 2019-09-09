93baa876ac98a9b80fd8fe00695e6c8ab055710d
Daniel Ching <carterbox@users.noreply.github.com>
2019-09-09 14:55:22 -0500

    REF: Add doctest to moebius

--- jeweler/moebius.py
+++ jeweler/moebius.py
@@ -1,4 +1,7 @@
-"""This code is contributed by Manish Shaw(manishshaw1)"""
+"""Implements the Moebius function.
+
+This code is contributed by Manish Shaw(manishshaw1).
+"""
 
 
 def is_prime(n):
@@ -12,30 +15,32 @@ def is_prime(n):
 
 
 def moebius(N):
-    """Python Program to evaluate Mobius
+    """Evaluate the Mobius function for N.
+
     M(N) = 1 if N = 1
     M(N) = 0 if any prime factor of N is contained twice
     M(N) = (-1)^(no of distinct prime factors)
+
+    >>> moebius(17)
+    -1
+    >>> moebius(25)
+    0
+    >>> moebius(6)
+    1
     """
     # Base Case
     if (N == 1):
         return 1
-
     # For a prime factor i check if i^2 is also a factor.
     p = 0
     for i in range(1, N + 1):
         if (N % i == 0 and is_prime(i)):
-
-            # Check if N is
-            # divisible by i^2
+            # Check if N is divisible by i^2
             if (N % (i * i) == 0):
                 return 0
             else:
-
-                # i occurs only once,
-                # increase f
+                # i occurs only once, increase f
                 p = p + 1
-
     # All prime factors are contained only once Return 1 if p is even else -1
     if (p % 2 != 0):
         return -1
@@ -43,7 +48,6 @@ def moebius(N):
         return 1
 
 
-if __name__ == '__main__':
-    print("Mobius defs M(N) at N = {} is: {}".format(17, mobius(17)), end="\n")
-    print("Mobius defs M(N) at N = {} is: {}".format(25, mobius(25)), end="\n")
-    print("Mobius defs M(N) at N = {} is: {}".format(6, mobius(6)), end="\n")
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
