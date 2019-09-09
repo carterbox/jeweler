abe1f5edd88016858419d785442eb42fc358e12b
Daniel Ching <carterbox@users.noreply.github.com>
2019-09-09 14:35:43 -0500

    TST: Add doctest for best function

--- jeweler/catalog/__init__.py
+++ jeweler/catalog/__init__.py
@@ -11,10 +11,19 @@ __all__ = [
 
 
 def best(L, objective_function, weight):
-    """Access best codes from previous searches."""
+    """Access best codes from previous searches.
+
+    >>> best(10, 'spectral_flatness', 5)
+    [0, 0, 0, 0, 1, 1, 0, 1, 1, 1]
+    """
     f = Archiver(output_dir=os.path.dirname(__file__))
     return f.fetch(
         L=L,
         objective_function=objective_function,
         weight=weight
     )['code']
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
