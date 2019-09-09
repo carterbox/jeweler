14850aab2e043b2ee5ee0d2dd949d640a5d168ac
Daniel Ching <carterbox@users.noreply.github.com>
2019-09-09 14:23:17 -0500

    API: Implement the best function for the catalog.

--- jeweler/catalog/__init__.py
+++ jeweler/catalog/__init__.py
@@ -1,12 +1,20 @@
 """This module contains a readable archive of the best codes."""
 
 import json
+import os
+
+from jeweler.io import Archiver
 
 __all__ = [
     'best'
 ]
 
 
-def best(L, objective_function, density):
+def best(L, objective_function, weight):
     """Access best codes from previous searches."""
-    raise NotImplementedError()
+    f = Archiver(output_dir=os.path.dirname(__file__))
+    return f.fetch(
+        L=L,
+        objective_function=objective_function,
+        weight=weight
+    )['code']
--- jeweler/io.py
+++ jeweler/io.py
@@ -30,6 +30,17 @@ all = [
 logger = logging.getLogger(__name__)
 
 
+class NotInCatalogError(ValueError):
+    """Raised when a code does not exist in the catalog."""
+
+    def __init__(self, L, weight, objective_function):
+        message = (
+            f"{weight}/{L} codes have not been evaluated "
+            f"with '{objective_function}'."
+        )
+        super(NotInCatalogError, self).__init__(message)
+
+
 class Archiver(object):
     """Manages code records for competing disk writes.
 
@@ -43,6 +54,7 @@ class Archiver(object):
     """
 
     def __init__(self, output_dir):
+        """Set up an Archiver instance."""
         self.output_dir = os.path.abspath(output_dir)
         # Create the file if it doesn't already exist
         os.makedirs(self.output_dir, exist_ok=True)
@@ -81,4 +93,27 @@ class Archiver(object):
             f.truncate()  # remove remaining part
             json.dump(data, f, indent=4)
             fcntl.flock(f, fcntl.LOCK_UN)
-        return
+
+    def fetch(
+            self,
+            L,
+            objective_function,
+            weight,
+    ):
+        """Get a code and its cost from the disk."""
+        W = str(weight)  # must be a string otherwise dupicate entries
+        filename = os.path.join(self.output_dir, f"{L}.json")
+        if not os.path.isfile(filename):
+            raise NotInCatalogError(L, W, objective_function)
+        with open(filename, 'r') as f:
+            try:
+                data = json.load(f)
+            except json.decoder.JSONDecodeError:
+                logger.warning(f"{filename} exists, "
+                               f"but it is improperly formatted.")
+                raise
+        if (W in data and objective_function in data[W]
+                and 'code' in data[W][objective_function]):
+            return data[W][objective_function]
+        else:
+            raise NotInCatalogError(L, W, objective_function)
