abc01b81183e881b548360e28daa885e14449988
Daniel Ching <carterbox@users.noreply.github.com>
2020-11-07 00:22:32 -0600

    DOC: Documentation and input checking

--- src/pybind11/bracelet.cxx
+++ src/pybind11/bracelet.cxx
@@ -3,21 +3,49 @@
 #include <pybind11/stl.h>
 #include <vector>
 
+extern "C" {
 #include "bracelet.h"
+}
+
+using namespace pybind11::literals;
 
 PYBIND11_MODULE(bracelet, m) {
-  m.doc() = "Generates combinatoric bracelets of fixed content using the "
-            "algorithm from Karim et al. (2012).\nKarim, S., "
-            "J. Sawada, Z. Alamgir, and S. M. Husnine. 2013. “Generating "
-            "Bracelets with Fixed Content.” Theoretical Computer Science 475: "
-            "103–12. https://doi.org/10.1016/j.tcs.2012.11.024.";
+  m.doc() = "Provides functions to generate combinatoric bracelets.";
 
   m.def(
       "bracelet_fc",
       [](int n, int k, const std::vector<int> &counts) {
-        int num[64];
-        std::copy(counts.begin(), counts.end(), num + 1);
+        int *num = new int[n + 1];
+        int total = 0;
+        for (int i = 0; i < k; i++) {
+          if (counts[i] <= 0) {
+            throw std::invalid_argument(
+                "All counts must be greater than zero.");
+          }
+          total += counts[i];
+          num[i + 1] = counts[i];
+        }
+        if (total != n) {
+          throw std::invalid_argument("The sum of counts must be n.");
+        }
         BraceletFC(n, k, num);
       },
-      "Print all bracelets of fixed content.");
+      R"(Print all bracelets of fixed content using method by Karim et al.
+
+    Parameters
+    ----------
+    n : int
+        The length of the bracelet.
+    k : int
+        The number of unique colors in the bracelet.
+    counts : [int]
+        The number of each unique color.
+
+    References
+    ----------
+    Karim, S., J. Sawada, Z. Alamgir, and S. M. Husnine. 2013. “Generating
+    Bracelets with Fixed Content.” Theoretical Computer Science 475: 103–12.
+    https://doi.org/10.1016/j.tcs.2012.11.024.
+)",
+      "n"_a, "k"_a, "counts"_a);
 }
