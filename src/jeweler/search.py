659101961af6aaf36081822352ebf54628c3fba2
Daniel Ching <carterbox@users.noreply.github.com>
2021-04-27 13:52:27 -0500

    NEW: Use SageMath to generate Necklaces of fixed-content

--- src/jeweler/search.py
+++ src/jeweler/search.py
@@ -16,6 +16,7 @@ import logging
 import os
 
 import numpy as np
+from sage.all import Necklaces
 from tqdm import tqdm
 
 from jeweler.bracelet import bracelet_fc
@@ -26,6 +27,7 @@ __all__ = [
     'lyndon',
     'bfs',
     'bracelet',
+    'necklace',
 ]
 
 logger = logging.getLogger(__name__)
@@ -181,6 +183,86 @@ def bracelet(
                 if scores[best] > score_best:
                     score_best = scores[best]
                     code_best = batch[best]
+
+            f.update(
+                objective_function.__name__,
+                code_best,
+                score_best,
+                weight=k,
+            )
+
+
+def _necklace_chunk(necklaces, chunksize):
+    """Wrap a sage.Necklaces instance in a Python generator.
+
+    Parameters
+    ----------
+    necklages : sage.Necklaces
+        A SageMath necklage class instance.
+    chunksize : 2-tuple
+        The shape of the desired chunk of necklaces.
+    """
+    assert chunksize[1] == len(necklaces.first())
+    chunk = np.empty(chunksize, dtype='float32')
+    i = 0
+    for x in necklaces:
+        chunk[i] = x
+        i += 1
+        if i >= chunksize[0]:
+            yield chunk - 1
+            chunk = np.empty(chunksize, dtype='float32')
+            i = 0
+    if i < chunksize[0]:
+        yield chunk[:i] - 1
+
+
+def necklace(
+    K,
+    L,
+    output_dir,
+    objective_function,
+    density=0.5,
+    batch_size=2048,
+):
+    """Search necklaces of length L and fixed content for the best binary code.
+
+    Parameters
+    ----------
+    L : int
+        The maximum code length
+    output_dir : path
+        Location to put the output files
+    objective_function : function
+        A function from jeweler.objective where better scores are larger
+    density : float
+        The sum of the code divided by the length of the code
+    """
+    logger.info(f"Fixed-content necklaces of length {K}..{L}.")
+    logger.info(f"the objective is '{objective_function.__name__}'.")
+    logger.info(f"code density is {density:g}.")
+
+    with Archiver(output_dir=output_dir) as f:
+
+        for L in range(K, L + 1):
+
+            logger.info(f"Generating necklaces of length {L}.")
+            k = int(L * density)  # number of 1s in the code
+
+            codes = Necklaces([L - k, k])
+            ncodes = codes.cardinality()
+            chunks = _necklace_chunk(codes, (batch_size, L))
+            logger.info(f"{ncodes:,d} necklaces discovered.")
+
+            title = f"fixed-content necklaces 1D {L:d}-bit code"
+            code_best = None
+            score_best = -np.inf
+            for _ in tqdm(range(ncodes // batch_size + 1), desc=title):
+                batch = next(chunks)
+                scores = objective_function(batch)
+                best = np.argmax(scores)
+                if scores[best] > score_best:
+                    score_best = scores[best]
+                    code_best = batch[best].astype('int', copy=False).tolist()
 
             f.update(
                 objective_function.__name__,
