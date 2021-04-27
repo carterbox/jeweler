e7e80efb5cadad5544c62467710d3c40ef2a8786
Daniel Ching <carterbox@users.noreply.github.com>
2021-04-27 14:28:26 -0500

    TST: Add profiler for necklace method

---
+++ test/profile-necklace.py
@@ -0,0 +1,25 @@
+import os
+import logging
+
+import jeweler
+
+logger = logging.basicConfig(level=logging.INFO)
+
+if __name__ == '__main__':
+    from pyinstrument import Profiler
+    profiler = Profiler()
+    profiler.start()
+    jeweler.search.necklace(
+        K=31,
+        L=31,
+        output_dir=os.path.dirname(jeweler.catalog.__file__),
+        objective_function=jeweler.objective.minimal_variance,
+    )
+    profiler.stop()
+    print(profiler.output_text(unicode=True, color=True))
+
+""" Checking the number of ones in the code to see if it matches the desired
+weight is most expensive. Try switching to necklaces of defined weight to
+avoid computing this sum. The second most expensive thing is the cost function.
+Surprisingly, the FFT is not as expensive as the mean, product, and variance
+operations combined."""
