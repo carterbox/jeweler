49e92ade5cc89dba2daf7f8462597d9b618f1eed
Daniel Ching <carterbox@users.noreply.github.com>
2021-07-15 17:07:48 -0500

    TST: Enforce invariance assumptions for objectives

--- test/test_invariance.py
+++ test/test_invariance.py
@@ -1,9 +1,11 @@
 import unittest
+from jeweler.objective import minimal_variance, spectral_flatness
 
 import numpy as np
 
 
-def invariant_function(x):
+def magnitude(x):
+    """Return the magnitude of the DFT of x."""
     return np.abs(np.fft.rfft(x, axis=-1))
 
 
@@ -11,24 +13,43 @@ class TestDFTInvarianceAssumptions(unittest.TestCase):
     """Check if the DFT magnitude is invariant to various transformations."""
     def setUp(self):
         super().setUp()
+        self.invariant_function = magnitude
         self.codes = np.array([
             [1, 0, 0, 0, 0, 0],
             [1, 0, 1, 0, 1, 0],
             [0, 0, 1, 1, 0, 1],
             [1, 1, 0, 1, 0, 0],
             [1, 1, 1, 1, 1, 1],
-            [0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 1],
         ])
-        self.ref = invariant_function(self.codes)
+
+    # @unittest.skip("For debugging purposes only.")
+    # def test_name_function(self):
+    #     print(self.invariant_function.__name__)
 
     def test_rotation_invariant(self):
+        ref = self.invariant_function(self.codes)
         for i in range(self.codes.shape[-1]):
-            mod = invariant_function(np.roll(self.codes, shift=i, axis=-1))
-            np.testing.assert_allclose(self.ref, mod)
+            mod = self.invariant_function(np.roll(self.codes, shift=i,
+                                                  axis=-1))
+            np.testing.assert_allclose(ref, mod, rtol=1)
 
     def test_reverse_invariant(self):
-        mod = invariant_function(np.flip(self.codes, axis=-1))
-        np.testing.assert_allclose(self.ref, mod)
+        ref = self.invariant_function(self.codes)
+        mod = self.invariant_function(np.flip(self.codes, axis=-1))
+        np.testing.assert_allclose(ref, mod, rtol=1)
+
+
+class TestSpectralFlatnessInvarianceAssumptions(TestDFTInvarianceAssumptions):
+    def setUp(self):
+        super().setUp()
+        self.invariant_function = spectral_flatness
+
+
+class TestMinimalVarianceInvarianceAssumptions(TestDFTInvarianceAssumptions):
+    def setUp(self):
+        super().setUp()
+        self.invariant_function = minimal_variance
 
 
 if __name__ == '__main__':
