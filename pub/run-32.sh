636054f267c7c4f51189087cb8177fc73f1e379b
Daniel Ching <carterbox@users.noreply.github.com>
2021-12-08 13:53:59 -0600

    DEV: Create some batch scripts for benchmarking

---
+++ pub/run-32.sh
@@ -0,0 +1,65 @@
+jeweler 32 32 -o random-0 -f spectral_flatness -s random -l run-32.log &
+jeweler 32 32 -o random-1 -f spectral_flatness -s random -l run-32.log &
+jeweler 32 32 -o random-2 -f spectral_flatness -s random -l run-32.log &
+jeweler 32 32 -o random-3 -f spectral_flatness -s random -l run-32.log &
+jeweler 32 32 -o random-4 -f spectral_flatness -s random -l run-32.log &
+jeweler 32 32 -o random-5 -f spectral_flatness -s random -l run-32.log &
+jeweler 32 32 -o random-6 -f spectral_flatness -s random -l run-32.log &
+jeweler 32 32 -o random-7 -f spectral_flatness -s random -l run-32.log &
+jeweler 32 32 -o random-8 -f spectral_flatness -s random -l run-32.log &
+jeweler 32 32 -o random-9 -f spectral_flatness -s random -l run-32.log &
+jeweler 32 32 -o random-10 -f spectral_flatness -s random -l run-32.log &
+jeweler 32 32 -o random-11 -f spectral_flatness -s random -l run-32.log &
+jeweler 32 32 -o random-12 -f spectral_flatness -s random -l run-32.log &
+jeweler 32 32 -o random-13 -f spectral_flatness -s random -l run-32.log &
+jeweler 32 32 -o random-14 -f spectral_flatness -s random  -l run-32.log &
+jeweler 32 32 -o random-15 -f spectral_flatness -s random  -l run-32.log &
+
+wait
+
+jeweler 32 32 -o random-0 -f minimal_variance -s random -l run-32.log &
+jeweler 32 32 -o random-1 -f minimal_variance -s random -l run-32.log &
+jeweler 32 32 -o random-2 -f minimal_variance -s random -l run-32.log &
+jeweler 32 32 -o random-3 -f minimal_variance -s random -l run-32.log &
+jeweler 32 32 -o random-4 -f minimal_variance -s random -l run-32.log &
+jeweler 32 32 -o random-5 -f minimal_variance -s random -l run-32.log &
+jeweler 32 32 -o random-6 -f minimal_variance -s random -l run-32.log &
+jeweler 32 32 -o random-7 -f minimal_variance -s random -l run-32.log &
+jeweler 32 32 -o random-8 -f minimal_variance -s random -l run-32.log &
+jeweler 32 32 -o random-9 -f minimal_variance -s random -l run-32.log &
+jeweler 32 32 -o random-10 -f minimal_variance -s random -l run-32.log &
+jeweler 32 32 -o random-11 -f minimal_variance -s random -l run-32.log &
+jeweler 32 32 -o random-12 -f minimal_variance -s random -l run-32.log &
+jeweler 32 32 -o random-13 -f minimal_variance -s random -l run-32.log &
+jeweler 32 32 -o random-14 -f minimal_variance -s random  -l run-32.log &
+jeweler 32 32 -o random-15 -f minimal_variance -s random  -l run-32.log &
+
+wait
+
+jeweler 32 32 -o random-0 -f coded_factor -s random -l run-32.log &
+jeweler 32 32 -o random-1 -f coded_factor -s random -l run-32.log &
+jeweler 32 32 -o random-2 -f coded_factor -s random -l run-32.log &
+jeweler 32 32 -o random-3 -f coded_factor -s random -l run-32.log &
+jeweler 32 32 -o random-4 -f coded_factor -s random -l run-32.log &
+jeweler 32 32 -o random-5 -f coded_factor -s random -l run-32.log &
+jeweler 32 32 -o random-6 -f coded_factor -s random -l run-32.log &
+jeweler 32 32 -o random-7 -f coded_factor -s random -l run-32.log &
+jeweler 32 32 -o random-8 -f coded_factor -s random -l run-32.log &
+jeweler 32 32 -o random-9 -f coded_factor -s random -l run-32.log &
+jeweler 32 32 -o random-10 -f coded_factor -s random -l run-32.log &
+jeweler 32 32 -o random-11 -f coded_factor -s random -l run-32.log &
+jeweler 32 32 -o random-12 -f coded_factor -s random -l run-32.log &
+jeweler 32 32 -o random-13 -f coded_factor -s random -l run-32.log &
+jeweler 32 32 -o random-14 -f coded_factor -s random  -l run-32.log &
+jeweler 32 32 -o random-15 -f coded_factor -s random  -l run-32.log &
+
+wait
+
+jeweler 32 32 -o lyndon-0 -f coded_factor -s lyndon -l run-32.log &
+jeweler 32 32 -o lyndon-0 -f spectral_flatness -s lyndon -l run-32.log &
+jeweler 32 32 -o lyndon-0 -f minimal_variance -s lyndon -l run-32.log &
+jeweler 32 32 -o exhaustive-0 -f coded_factor -s exhaustive -l run-32.log &
+jeweler 32 32 -o exhaustive-0 -f spectral_flatness -s exhaustive -l run-32.log &
+jeweler 32 32 -o exhaustive-0 -f minimal_variance -s exhaustive -l run-32.log &
+
+wait
